6cfb943b17c3ab9cc5dd204246817c85f9740623
NicolasLoth <lothnic1@students.zhaw.ch>
2017-03-11 11:13:33 +0100

    Added DAL

---
+++ TravelBuddy5.DAL/ITestRepo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TravelBuddy5.DAL
+{
+    public interface ITestRepo
+    {
+
+        int GetTestNumber();
+        void SetTestNumber(int number);
+
+    }
+}
